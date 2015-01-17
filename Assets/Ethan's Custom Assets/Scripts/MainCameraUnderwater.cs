fdd2806e4895446897999a36485d6cbcdda5de98
Ethan Currah <ECurrah90@gmail.com>
2015-01-16 19:36:08 -0500

    This is a Project I have been working on. I am opening it up for us to play around with

---
+++ Assets/Ethan's Custom Assets/Scripts/MainCameraUnderwater.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCameraUnderwater : MonoBehaviour {
+
+	//Hold the scenes 'out-of-water' settings
+	private bool defaultFog;
+	private Color defaultFogColor;
+	private float defaultFogDensity;
+	private Material defaultSkybox;
+	private Material noSkybox;
+
+	public float underwaterFogDensity;
+
+	void Awake(){
+		defaultFog = RenderSettings.fog;
+		defaultFogColor = RenderSettings.fogColor;
+		defaultFogDensity = RenderSettings.fogDensity;
+		defaultSkybox = RenderSettings.skybox;
+	}
+	
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag ("MainCamera")) {
+			Debug.Log ("MainCamera Entered the Water.");
+			RenderSettings.fog = true;
+			RenderSettings.fogColor = new Color (0, 0.4f, 0.7f, 0.6f);
+			RenderSettings.fogDensity = underwaterFogDensity;
+			RenderSettings.skybox = noSkybox;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.CompareTag ("MainCamera")) {
+			Debug.Log ("MainCamera Left the Water.");
+			RenderSettings.fog = defaultFog;
+			RenderSettings.fogColor = defaultFogColor;
+			RenderSettings.fogDensity = defaultFogDensity;
+			RenderSettings.skybox = defaultSkybox;
+		}
+	}
+}
---
+++ Assets/Ethan's Custom Assets/Scripts/PlayerInWaterScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInWaterScript : MonoBehaviour {
+	public float underwaterGravity;
+	//Create reference for the scene's 'out-of-water' settings
+	void Awake(){
+		//Hold default underwater settings
+
+	}
+	
+	
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag ("Player")) {
+			//Change settings for when player is in the water
+			Debug.Log ("Player Entered the Water.");
+			CharacterMotor motor = other.GetComponent <CharacterMotor>();
+			motor.movement.gravity *= .001f;
+			motor.movement.velocity = new Vector3(motor.movement.velocity.x * 0.1f, motor.movement.velocity.y * 0.1f, motor.movement.velocity.z * 0.1f);
+		}
+	}
+	
+	void OnTriggerExit(Collider other) {
+		if (other.CompareTag ("Player")) {
+			//Change settings for when player is out of water
+			Debug.Log ("Player Left the Water.");
+			CharacterMotor motor = other.GetComponent <CharacterMotor>();
+			motor.movement.gravity *= 1000f;
+		}
+	}
+}
